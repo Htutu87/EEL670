501ad7706be0d6f84968a2a9ce8a15df64ec2971
Htutu87 <artur_amaral@poli.ufrj.br>
2021-08-25 14:45:34 -0300

    Estrutura do projeto e makefile prontos

---
+++ grafo/src/main.cpp
@@ -0,0 +1,10 @@
+// EEL670 - Linguagens de programação
+// Período 2021.1 - Laboratório 01 - Exercício avaliado 02
+// Autor: Artur Amaral
+
+#include "../include/grafo.h"
+
+int main () 
+{
+    return 0;
+}
